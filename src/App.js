dd951477734f79755b18e9878071d6366a47d32c
Andre Lee <andreleecoder@gmail.com>
2019-03-25 23:39:35 -0300

    last commit of the night

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {components} from 'react';
+import components from 
+'./components/components1'
+import 'App.css';
+class App extends components {
+    render(){ 
+      return(
+    <div className="App">
+     <components1/>
+    </div>
+      );
+    }
+ }
+
+ export default App;
+
---
+++ src/components/components1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function components() {
+  return (
+    <div>
+         <h1>component 1</h1> 
+     <p>Loremjsgqvcyqwgv qgvvwjvcjqh whbsd  qjbhhkbcjja qkjwbckjbkb kjqbk kwhjwbcqwh kiwqsjbck
+
+     </p>
+    </div>
+  )
+}
+
