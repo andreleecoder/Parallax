a791a704913347f4f40042f5df0f4d5848ac4c8a
Andre Lee <andreleecoder@gmail.com>
2019-03-27 00:01:05 -0300

    almost nothing done but a need to sleep

--- App.js
+++ App.js
@@ -7,8 +7,10 @@ principal.addEventListener("scroll", scrollY);
 
 function scrollY(){
     if (principal== scrollY) {
-        
+        ActiveXObject.fadeIn
         
     }
 
 }
+
+//https://www.w3schools.com/howto/howto_js_navbar_slide.asp
